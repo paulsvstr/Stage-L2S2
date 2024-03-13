b48be5ef47cfaad2008c327d63bfbfae2010d293
paulsvstr <paulsevestre@MacBook-Pro-de-Paul.local>
2024-03-13 16:20:34 +0100

    mise en place d'un nouvel algo

---
+++ GTW2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 16:18:07 2024
+
+@author: paulsevestre
+"""
+
+from rdflib import Graph, Namespace
+from rdflib.plugins.sparql import prepareQuery
+
+# Charger la base de données YAGO Tiny depuis un fichier Turtle
+print("tout est bien importé")
+
+yago_graph = Graph()
+
+print("Graph() executed")
+
+yago_graph.parse("yago-tiny.ttl", format="ttl")
+
+print("Yago graph parse bien executed")
+
+# Définir les préfixes pour les namespaces utilisés dans YAGO
+YAGO = Namespace("http://yago-knowledge.org/resource/")
+RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+
+print("pref bien def")
+
+def trouver_type_mot(mot):
+    # Requête SPARQL pour trouver le type du mot donné
+    query = prepareQuery(
+        """
+        SELECT DISTINCT ?type
+        WHERE {
+            YAGO:%s rdf:type ?type .
+        }
+        """ % mot,
+        initNs={"YAGO": YAGO, "RDF": RDF}
+    )
+
+    # Exécuter la requête SPARQL
+    results = yago_graph.query(query)
+
+    # Récupérer et retourner le type du mot
+    types = [row.type for row in results]
+    return types
+
+# Mot à rechercher
+mot_a_rechercher = "Elvis_Presley"  # Vous pouvez changer le mot ici
+
+# Trouver et afficher le type du mot
+types = trouver_type_mot(mot_a_rechercher)
+if types:
+    print(f"Le type de '{mot_a_rechercher}' est : {types}")
+else:
+    print(f"Le type de '{mot_a_rechercher}' n'a pas été trouvé dans la base de données YAGO Tiny.")
