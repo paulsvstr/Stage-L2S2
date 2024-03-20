451ea62e3679f88b4feb32f922958bb65214db50
paulsvstr <paulsevestre@MacBook-Pro-de-Paul.local>
2024-03-20 14:58:11 +0100

    autre test

--- test_parse.py
+++ test_parse.py
@@ -5,12 +5,19 @@ Created on Wed Mar 20 14:43:37 2024
 
 @author: paulsevestre
 """
-
+import time
 from rdflib import Graph
 
 yago_graph = Graph()
 
+start = time.time()
 
 yago_graph.parse("/home/psevestre/YagotinyKB/yago-tiny50000.ttl", format="ttl")
 
+end = time.time()
+
 print("Yago graph parse bien executed")
+
+elapsed = end - start
+
+print(f'Temps d\'exécution : {elapsed:.2}ms')
