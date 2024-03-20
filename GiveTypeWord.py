f2626012add652e84bc448a741bebec13e48d845
paulsvstr <paulsevestre@MacBook-Pro-de-Paul.local>
2024-03-20 17:33:34 +0100

    vf

--- GiveTypeWord.py
+++ GiveTypeWord.py
@@ -10,9 +10,8 @@ print('programme bien lancé et modif')
 # Charger la base de données YAGO Tiny
 yago_graph = Graph(store="BerkeleyDB")
 print("commande graph... ok")
-yago_graph.open("yago.db",create=True)
+yago_graph.open("yago.db")
 print('accès à yagodb OK')
-yago_graph.parse("/home/psevestre/YagotinyKB/yago-tiny.ttl", format="ttl")
 print('yago_graph.parse OK')
 
 def trouver_type_mot(mot):
@@ -36,7 +35,7 @@ def trouver_type_mot(mot):
     return types
 
 # Mot à rechercher
-mot_a_rechercher = "Elvis_Presley"  # Vous pouvez changer le mot ici
+mot_a_rechercher = "Elvis Presley"  # Vous pouvez changer le mot ici
 
 # Trouver et afficher le type du mot
 types = trouver_type_mot(mot_a_rechercher)
